e5f088ae4bc6700cc47119e4815f6b18431c8403
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2025-06-06 15:25:18 -0700

    Remove sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
-import { withSentryConfig } from "@sentry/nextjs";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-const config = withSentryConfig(nextConfig, {
-    org: "tinfoil",
-    project: "landing-site",
-    // Only print logs for uploading source maps in CI
-    // Set to `true` to suppress logs
-    silent: !process.env.CI,
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-});
-
-export default config;
+export default nextConfig;
