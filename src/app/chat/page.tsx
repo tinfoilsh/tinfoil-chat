9d4bc9199482efc173c0e7cba28e9a929a2b51ae
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2025-06-06 22:31:31 -0700

    fix build

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,6 +2,8 @@
 
 import { ChatWrapper } from '@/components/chat/chat-wrapper'
 
+export const dynamic = 'force-dynamic'
+
 export default function Chat() {
   return (
     <div className="h-screen">
