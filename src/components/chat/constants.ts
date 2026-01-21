803b5f1efbdc43667d00573d5e57e94af433b212
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2026-01-20 16:11:54 -0800

    chore: update websearch url

--- src/components/chat/constants.ts
+++ src/components/chat/constants.ts
@@ -1,4 +1,5 @@
-export const WEB_SEARCH_ENCLAVE_URL = 'https://websearch.tinfoil.sh'
+export const WEB_SEARCH_ENCLAVE_URL =
+  'https://websearch.tinfoil.functions.tinfoil.sh'
 
 export const CONSTANTS = {
   LOADING_TIMEOUT: 500,
